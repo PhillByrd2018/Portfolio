9fde4719c6c748d5e0c00dcc6383c19414deeb55
Phill_Byrd2018 <31040339+PhillByrd2018@users.noreply.github.com>
2017-10-28 12:17:01 -0400

    Add files via upload

---
+++ JavaScript/JOSN/ajax.php
@@ -0,0 +1,18 @@
+<? php
+$q = strtolower($_GET['q'});
+
+if(strcasecmp($q, 'm') ==0{
+ echo <a href="http://compsci.cofc.edu/about/faculty-staff-listing/moore-christine.php">Home</a>;
+ echo <a href="http://compsci.cofc.edu/about/faculty-staff-listing/moore-christine.php">Home</a>;
+ echo <a href="http://compsci.cofc.edu/about/faculty-staff-listing/moore-christine.php">Home</a>;
+ echo <a href="http://compsci.cofc.edu/about/faculty-staff-listing/moore-christine.php">Home</a>;
+}else if(strcasecmp($q, 'mo'){
+     echo <a href="http://compsci.cofc.edu/about/faculty-staff-listing/moore-christine.php">Home</a>;
+     echo <a href="http://compsci.cofc.edu/about/faculty-staff-listing/moore-christine.php">Home</a>;
+     echo <a href="http://compsci.cofc.edu/about/faculty-staff-listing/moore-christine.php">Home</a>;
+     echo <a href="http://compsci.cofc.edu/about/faculty-staff-listing/moore-christine.php">Home</a>;
+     echo <a href="http://compsci.cofc.edu/about/faculty-staff-listing/moore-christine.php">Home</a>;
+
+}
+
+?>
